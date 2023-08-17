2f67fa6cd9ed4950536a4e16b00f4a7ee019e76c
Abdul Rehman Amjad <abdulrehman.amjad@systemsltd.com>
2023-08-17 14:05:02 +0500

    - Smiles Exploer Home setup - Sections API implementation - Home Sections setup - Reward points API implementation

--- Sources/SmilesExplorer/Helpers/TableViewDataSource/TableViewDataSource.swift
+++ Sources/SmilesExplorer/Helpers/TableViewDataSource/TableViewDataSource.swift
@@ -32,23 +32,23 @@ extension TableViewDataSource where Model == OfferDO {
     }
 }
 
-extension TableViewDataSource where Model == GetTopOffersResponseModel {
-    static func make(forTopOffers collectionsObject: GetTopOffersResponseModel,
-                     reuseIdentifier: String = "TopOffersTableViewCell", data : String, isDummy:Bool = false, completion:((GetTopOffersResponseModel.TopOfferAdsDO) -> ())?) -> TableViewDataSource {
-        return TableViewDataSource(
-            models: [collectionsObject].filter({$0.ads?.count ?? 0 > 0}),
-            reuseIdentifier: reuseIdentifier,
-            data: data,
-            isDummy: isDummy
-        ) { (topOffers, cell, data, indexPath) in
-            guard let cell = cell as? TopOffersTableViewCell else {return}
-            cell.showPageControl = false
-            cell.sliderTimeInterval = topOffers.sliderTimeout
-            cell.collectionsData = topOffers.ads
-            cell.setBackGroundColor(color: UIColor(hexString: data))
-            cell.callBack = { data in
-                completion?(data)
-            }
-        }
-    }
-}
+//extension TableViewDataSource where Model == GetTopOffersResponseModel {
+//    static func make(forTopOffers collectionsObject: GetTopOffersResponseModel,
+//                     reuseIdentifier: String = "TopOffersTableViewCell", data : String, isDummy:Bool = false, completion:((GetTopOffersResponseModel.TopOfferAdsDO) -> ())?) -> TableViewDataSource {
+//        return TableViewDataSource(
+//            models: [collectionsObject].filter({$0.ads?.count ?? 0 > 0}),
+//            reuseIdentifier: reuseIdentifier,
+//            data: data,
+//            isDummy: isDummy
+//        ) { (topOffers, cell, data, indexPath) in
+//            guard let cell = cell as? TopOffersTableViewCell else {return}
+//            cell.showPageControl = false
+//            cell.sliderTimeInterval = topOffers.sliderTimeout
+//            cell.collectionsData = topOffers.ads
+//            cell.setBackGroundColor(color: UIColor(hexString: data))
+//            cell.callBack = { data in
+//                completion?(data)
+//            }
+//        }
+//    }
+//}
--- Sources/SmilesExplorer/Models/File.swift
+++
@@ -1,8 +0,0 @@
-//
-//  File.swift
-//  
-//
-//  Created by Abdul Rehman Amjad on 15/08/2023.
-//
-
-import Foundation
