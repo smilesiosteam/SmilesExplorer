30b76bb0210b52d5487dbd4f85b98a190a47a5f6
SmilesDev <smilesiosteam@gmail.com>
2023-08-18 12:31:07 +0500

    feat: Added SmilesExplorerHomeTicketsTableViewCell and SmilesExplorerDealsAndOffersTableViewCell
    - Added two new cell types for distinct content presentation.
    - Designed cells with consistent styling, spacing, and dimensions.

---
+++ Sources/SmilesExplorer/Views/Cells/CollectionViewCells/SmilesExplorerDealsAndOffersTableViewCell/SmilesExplorerDealsAndOffersTableViewCell.swift
@@ -0,0 +1,31 @@
+//
+//  SmilesExplorerDealsAndOffersTableViewCell.swift
+//  
+//
+//  Created by Ghullam  Abbas on 18/08/2023.
+//
+
+import UIKit
+
+class SmilesExplorerDealsAndOffersTableViewCell: UICollectionViewCell {
+    //MARK: - IBOutlets -
+    @IBOutlet weak var imageContainerView: UIView!
+    @IBOutlet weak var brandLogoImageView: UIImageView!
+    @IBOutlet weak var brandTitleLabel: UILabel!
+    
+    //MARK: - Variables
+    //MARK: - CellView LifeCycel
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        setupUI()
+        // Initialization code
+    }
+    //MARK: - Helper Function
+    private func setupUI() {
+        self.imageContainerView.addMaskedCorner(withMaskedCorner: [.layerMinXMinYCorner, .layerMinXMaxYCorner, .layerMaxXMinYCorner, .layerMaxXMaxYCorner], cornerRadius: 12.0)
+        self.brandTitleLabel.fontTextStyle = .smilesTitle2
+       
+        self.brandTitleLabel.textColor = .appRevampLocationTextColor
+        
+    }
+}
---
+++ Sources/SmilesExplorer/Views/Cells/CollectionViewCells/SmilesExplorerHomeTicketsCollectionViewCell/SmilesExplorerHomeTicketsCollectionViewCell.swift
@@ -0,0 +1,35 @@
+//
+//  SmilesExplorerHomeTicketsCollectionViewCell.swift
+//  
+//
+//  Created by Ghullam  Abbas on 18/08/2023.
+//
+
+import UIKit
+
+class SmilesExplorerHomeTicketsCollectionViewCell: UICollectionViewCell {
+    //MARK: - IBOutlets -
+    @IBOutlet weak var imageContainerView: UIView!
+    @IBOutlet weak var brandLogoImageView: UIImageView!
+    @IBOutlet weak var brandTitleLabel: UILabel!
+    @IBOutlet weak var typeLabel: UILabel!
+    @IBOutlet weak var amountLabel: UILabel!
+    //MARK: - Variables
+    //MARK: - CellView LifeCycel
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        setupUI()
+        // Initialization code
+    }
+    //MARK: - Helper Function
+    private func setupUI() {
+        self.imageContainerView.addMaskedCorner(withMaskedCorner: [.layerMinXMinYCorner, .layerMinXMaxYCorner, .layerMaxXMinYCorner, .layerMaxXMaxYCorner], cornerRadius: 12.0)
+        self.brandTitleLabel.fontTextStyle = .smilesTitle2
+        self.typeLabel.fontTextStyle = .smilesLabel2
+        self.amountLabel.fontTextStyle = .smilesLabel1
+        self.brandTitleLabel.textColor = .appRevampLocationTextColor
+        self.typeLabel.textColor = .appRevampSubtitleColor
+        self.amountLabel.textColor = .appRevampSubtitleColor
+        self.amountLabel.attributedText = "".strikoutString(strikeOutColor: .appGreyColor_128)
+    }
+}
