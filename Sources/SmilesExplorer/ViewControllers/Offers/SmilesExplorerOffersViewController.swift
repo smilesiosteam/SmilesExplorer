5592f070b7d51538cfeeccf795036211652615d0
Ghullam Abbas <smilesiosteam@gmail.com>
2023-10-16 17:53:21 +0500

    fix: update ui in offers view controller

--- Sources/SmilesExplorer/ViewControllers/Offers/SmilesExplorerOffersViewController.swift
+++ Sources/SmilesExplorer/ViewControllers/Offers/SmilesExplorerOffersViewController.swift
@@ -123,6 +123,7 @@ public class SmilesExplorerOffersViewController: UIViewController {
                     }
                     response.offers?.append(contentsOf: self?.response?.offers ?? [])
                     self?.response = response
+                    self?.setupUI()
                     self?.hasMoreData = self?.response?.offersCount ?? 0 > self?.response?.offers?.count ?? 0
                     self?.isLoading = false
                     self?.currentPage += 1
