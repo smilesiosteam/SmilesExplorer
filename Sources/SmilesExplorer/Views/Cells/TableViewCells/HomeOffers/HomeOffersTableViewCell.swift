6fa804fda764a7315df299216802c92fb1c8784e
Abdul Rehman Amjad <abdulrehman.amjad@systemsltd.com>
2024-02-27 13:07:19 +0500

    fix: Fixed offer cell text alignment issue

--- Sources/SmilesExplorer/Views/Cells/TableViewCells/HomeOffers/HomeOffersTableViewCell.swift
+++ Sources/SmilesExplorer/Views/Cells/TableViewCells/HomeOffers/HomeOffersTableViewCell.swift
@@ -105,6 +105,6 @@ extension HomeOffersTableViewCell: UICollectionViewDelegate, UICollectionViewDat
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return CGSize(width: 135, height: 200)
+        return CGSize(width: 135, height: 204)
     }
 }
